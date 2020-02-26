7c6a7e2b9a72ac6a891d6b94ea496fb2a45ef9b0
AndrewR-96 <60441123+AndrewR-96@users.noreply.github.com>
2020-02-26 12:36:31 -0500

    Add files via upload

---
+++ Print5to99.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @author Andrew
+ * @created 02/26/2020
+ * @description Print all numbers between 5 to 99
+ */
+
+public class Print5to99 {
+	
+	public static void main( String[] args) {
+		int start = 5;
+		while(start <= 99) {
+			System.out.println(start);
+			start++;
+		}
+	}
+
+}
